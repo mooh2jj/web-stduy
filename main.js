832c28d39fa935d9692e584138cb67681c1b7953
mooh2jj@naver.com <62453668+mooh2jj@users.noreply.github.com>
2021-03-15 22:28:35 +0900

    정적파일 추가

---
+++ main.js
@@ -0,0 +1,18 @@
+// if(10 > 5){
+//     alert("true");
+// }else if(10 < 5){
+//     alert("false");
+// } else {
+//     alert("else!");
+// }
+
+// for(var i = 0; i < 10; i++){
+//     for(var j = 0; j < 10; j++)
+//         console.log(i+"*"+j+"="+ i*j);
+// }
+
+let str = "웹개발자-사관학교";
+let str_arr = str.split("-");
+console.log(str_arr);
+let str_str = str_arr.join("*");
+console.log(str_str);
